19cbebe6449011992916614eccb2807904694d17
janicka <wjanicka@agh.edu.pl>
2025-03-18 21:10:47 +0100

    Fix bugs

--- api/v1/swift_handler.go
+++ api/v1/swift_handler.go
@@ -3,40 +3,35 @@ package v1
 import (
 	"net/http"
 	"strings"
-	"swift-app/database"
 	"swift-app/internal/models"
+	"swift-app/internal/services"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func GetSwiftCode(c *gin.Context) {
 	swiftCode := c.Param("swift-code")
 	swiftCode = strings.ToUpper(swiftCode)
 
-	collection := database.GetCollection()
-
-	var result models.SwiftCode
-	err := collection.FindOne(c, bson.M{"swiftCode": swiftCode}).Decode(&result)
+	swift, err := services.FindSwiftByCode(swiftCode)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "SWIFT code not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		return
 	}
+
 	response := models.SwiftResponse{
-		Address:       result.Address,
-		BankName:      result.BankName,
-		CountryISO2:   result.CountryISO2,
-		CountryName:   result.CountryName,
-		IsHeadquarter: result.IsHeadquarter,
-		SwiftCode:     result.SwiftCode,
+		Address:       swift.Address,
+		BankName:      swift.BankName,
+		CountryISO2:   swift.CountryISO2,
+		CountryName:   swift.CountryName,
+		IsHeadquarter: swift.IsHeadquarter,
+		SwiftCode:     swift.SwiftCode,
 	}
 
-	if result.IsHeadquarter && result.Branches != nil && len(*result.Branches) > 0 {
+	if swift.IsHeadquarter && swift.Branches != nil && len(*swift.Branches) > 0 {
 		var branches []models.SwiftBranchResp
-		for _, branchSwiftCode := range *result.Branches {
-			var branch models.SwiftCode
-			err := collection.FindOne(c, bson.M{"swiftCode": branchSwiftCode}).Decode(&branch)
+		for _, branchSwiftCode := range *swift.Branches {
+			branch, err := services.FindSwiftByCode(branchSwiftCode)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch branch data"})
 				return
@@ -54,45 +49,14 @@ func GetSwiftCode(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
 func GetSwiftCodesByCountry(c *gin.Context) {
 	countryISO2 := c.Param("countryISO2code")
 	countryISO2 = strings.ToUpper(countryISO2)
-	collection := database.GetCollection()
 
-	var countryResult models.SwiftCode
-	err := collection.FindOne(c, bson.M{"countryISO2": countryISO2}).Decode(&countryResult)
+	swiftCodes, countryName, err := services.FindSwiftCodesByCountry(countryISO2)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch country name"})
-		return
-	}
-	countryName := countryResult.CountryName
-
-	cursor, err := collection.Find(c, bson.M{"countryISO2": countryISO2}, options.Find())
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch SWIFT codes"})
-		return
-	}
-	defer cursor.Close(c)
-
-	var swiftCodes []models.SwiftBranchResp
-	for cursor.Next(c) {
-		var swift models.SwiftCode
-		if err := cursor.Decode(&swift); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode SWIFT data"})
-			return
-		}
-
-		swiftCodes = append(swiftCodes, models.SwiftBranchResp{
-			Address:       swift.Address,
-			BankName:      swift.BankName,
-			CountryISO2:   swift.CountryISO2,
-			IsHeadquarter: swift.IsHeadquarter,
-			SwiftCode:     swift.SwiftCode,
-		})
-	}
-
-	if len(swiftCodes) == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "No SWIFT codes found for this country"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
--- internal/services/swift_service.go
+++ internal/services/swift_service.go
@@ -24,3 +24,43 @@ func FindSwiftByCode(code string) (*models.SwiftCode, error) {
 
 	return &swift, nil
 }
+
+func FindSwiftCodesByCountry(countryISO2 string) ([]models.SwiftBranchResp, string, error) {
+	collection := database.GetCollection()
+
+	var countryResult models.SwiftCode
+	err := collection.FindOne(context.Background(), bson.M{"countryISO2": countryISO2}).Decode(&countryResult)
+	if err != nil {
+		return nil, "", fmt.Errorf("Failed to fetch country name")
+	}
+
+	countryName := countryResult.CountryName
+
+	cursor, err := collection.Find(context.Background(), bson.M{"countryISO2": countryISO2})
+	if err != nil {
+		return nil, "", fmt.Errorf("Failed to fetch SWIFT codes")
+	}
+	defer cursor.Close(context.Background())
+
+	var swiftCodes []models.SwiftBranchResp
+	for cursor.Next(context.Background()) {
+		var swift models.SwiftCode
+		if err := cursor.Decode(&swift); err != nil {
+			return nil, "", fmt.Errorf("Failed to decode SWIFT data")
+		}
+
+		swiftCodes = append(swiftCodes, models.SwiftBranchResp{
+			Address:       swift.Address,
+			BankName:      swift.BankName,
+			CountryISO2:   swift.CountryISO2,
+			IsHeadquarter: swift.IsHeadquarter,
+			SwiftCode:     swift.SwiftCode,
+		})
+	}
+
+	if len(swiftCodes) == 0 {
+		return nil, "", fmt.Errorf("No SWIFT codes found for this country")
+	}
+
+	return swiftCodes, countryName, nil
+}
